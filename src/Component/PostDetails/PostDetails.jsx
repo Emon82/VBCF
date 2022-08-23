1612798f4fe8b478eec05ee71c03f222f637be9b
Naimul Ferdous <ferdousnaimul@gmail.com>
2022-08-23 17:37:49 +0600

    more codings

---
+++ src/Component/PostDetails/PostDetails.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useParams } from "react-router";
+import "./PostDetails.css";
+
+import { useEffect, useState } from "react";
+
+const PostDetails = () => {
+  let { id } = useParams();
+
+  const [singlePost, setSinglePost] = useState({});
+
+  useEffect(() => {
+    fetch("http://192.168.0.147:8000/api/posts/" + id)
+      .then((response) => response.json())
+      .then((data) => setSinglePost(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="container py-3">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="w-100">
+              <img className="img-fluid w-100" src={singlePost.image} alt="" />
+            </div>
+          </div>
+          <div className="col-md-4">
+            <h1>{singlePost.title}</h1>
+            <p>{singlePost.content}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDetails;
---
+++ src/Component/Program/Program.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Program.css";
+
+export default function Program() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://192.168.0.147:8000/api/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <div className="container py-5">
+        <div class="row gy-3 justify-content-center">
+          <div className="col-md-7">
+            <div class="card post-card">
+              <div class="card-body">
+                <div className="card-title">
+                  <h2>Posts</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          {posts && posts.length ? (
+            posts.map((post) => (
+              <div className="col-md-7">
+                <div class="card post-card">
+                  <div class="card-header">
+                    <Link to={"/postDetails/" + post.id}>{post.title}</Link>
+                  </div>
+                  <div class="card-body">
+                    <div className="card-title">{post.content}</div>
+                    <Link to={"/postDetails/" + post.id}>
+                      <img
+                        // style={{ width: "100%", height: "450px" }}
+                        className="img-fluid w-100"
+                        src={post.image}
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div class="card-footer text-muted mt-5">
+                    Date: {post.updated_at.split("T")[0]}
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <h1>Loding </h1>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
